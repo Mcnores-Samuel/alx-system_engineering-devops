3aefb67119df5194ac08d9b0021f0cc3239befd1
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-27 19:02:42 +0200

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,49 @@
+$nginx = 'nginx'
+
+package { $nginx:
+  ensure   => installed,
+  provider => 'apt',
+}
+
+service { $nginx:
+  ensure  => running,
+  enable  => true,
+  require => Package[$nginx],
+}
+
+file { '/var/www/nginx-html':
+  ensure  => 'directory',
+  require => Package[$nginx],
+}
+
+file { '/var/www/nginx-html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+file { '/var/www/nginx-html/404.html':
+  ensure  => present,
+  content => 'Ceci n\'est pas une page',
+}
+
+file {
+  '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => "server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      root /var/www/nginx-html;
+      index index.html index.htm;
+
+      location /redirect_me {
+        return 301 http://www.google.com;
+      }
+
+      error_page 404 /404.html;
+      location /404 {
+          internal;
+      }
+    }",
+    require => Package[$nginx],
+    notify  => Service[$nginx],
+}
