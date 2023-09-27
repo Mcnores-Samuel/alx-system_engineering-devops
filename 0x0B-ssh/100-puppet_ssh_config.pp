574f73c44b74bd03a24a1cdad743fc9ad186476f
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-09-27 18:45:28 +0200

    update puppet file'

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,17 @@
 # Sets up client SSH configuration file so that it can connect to a server without typing a password.
 # Requirements:
 #   private key ~/.ssh/school
-#   configured to refuse to authenticate using a password
-
-class ssh_config {
-  file {
+#   configured to refuse to authenticate using a passwor
+file {
     '/etc/ssh/ssh_config':
     path => '/etc/ssh/ssh_config',
     ensure => present,
     line => "IdentityFile ~/.ssh/school"
-  }
+}
 
-  file {
+file {
     '/etc/ssh/sshd_config':
     path => '/etc/ssh/sshd_config',
     ensure => present,
     line => "PasswordAuthentication no"
-  }
 }
