e7f5718029d7a8908cc1375eb1b12f128e70f5a5
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-26 03:21:25 +0200

    export data in the JSON format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""exports response data to a json file"""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    username = user.get('username')
+
+    obj = {}
+    value_list = []
+    for todo in todos:
+        dict_d = {}
+        dict_d["task"] = todo.get('title')
+        dict_d["completed"] = todo.get('completed')
+        dict_d["username"] = username
+        value_list.append(dict_d)
+    obj["{}".format(user.get('id'))] = value_list
+    with open("{}.json".format(user.get('id')), 'w') as file:
+        json.dump(obj, file)
