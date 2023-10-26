72cac464758c42cd0aa4c7df732c05873396e0f2
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-26 03:44:29 +0200

    export data in the JSON format.

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Exports data in the JSON format."""
+import json
+import requests
+
+if __name__ == "__main__":
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+
+    obj = {}
+    value_list = []
+    for user in users:
+        for todo in todos:
+            if todo.get('userId') == user.get('id'):
+                dict_d = {}
+                dict_d["task"] = todo.get('title')
+                dict_d["completed"] = todo.get('completed')
+                dict_d["username"] = user.get('username')
+                value_list.append(dict_d)
+        obj["{}".format(user.get('id'))] = value_list
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(obj, file)
