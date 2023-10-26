dcbed0fa95d6702ff78052d2110f8540df8e5f48
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-26 02:35:59 +0200

    returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 """Returns information about his/her TODO list progress."""
+import requests
+import sys
+
 if __name__ == "__main__":
-    import requests
-    import sys
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    user_id = sys.argv[1]
-    user_data = requests.get(
-        'https://jsonplaceholder.typicode.com/users/?id={}'.format(user_id))
-    user_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(
-            user_id))
-
-    count = 0
-    user_data = user_data.json()
-    user_todos = user_todos.json()
-
-    for todo in user_todos:
-        if todo.get('completed'):
-            count += 1
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user_data[0].get('username'), count, len(user_todos)))
-    for todo in user_todos:
-        if todo.get('completed'):
-            print("\t {}".format(todo.get('title')))
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
