8c985449d8fcb6705843fbde302d0f7db8046584
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-01-18 23:37:34 +0200

    queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to request top 10 hot posts in a subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """Function to request top 10 hot posts in a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data").get("children")
+    [print(c.get("data").get("title")) for c in results.get("children")]
